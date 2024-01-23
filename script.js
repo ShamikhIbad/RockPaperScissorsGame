11e85c68c6107d8f41360b149345172c3d652ee2
Shamikh Ibad <shamikhibad3@gmail.com>
2024-01-24 01:46:39 +0500

    media querries added

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
-const msg = document.querySelector("#msg")
-const userScorePara = document.querySelector("#user-score")
-const compScorePara = document.querySelector("#comp-score")
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+
 
 const gerCompChoice = ()=>{
     const options = ["rock","paper","scissor"];
